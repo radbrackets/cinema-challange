12e0f28cc24104ee51aff274afdcd25fbd03018a
pskarbek <pawel@skarbek.net>
2022-03-21 09:19:33 +0100

    Init spring gradle project

---
+++ src/test/java/net/skarbek/cinemachallange/CinemaChallangeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.skarbek.cinemachallange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaChallangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
